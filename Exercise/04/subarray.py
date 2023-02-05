5e5b44aa68c72334f1a35eab4d8076a5a52d91e0
Omkar-Savoikar <mca.2104@unigoa.ac.in>
2023-02-06 04:03:35 +0530

    Refator Exercise 04

--- Exercise/04/subarray.py
+++ Exercise/04/subarray.py
@@ -1,25 +1,20 @@
-from data import arr, k
-
 class SubArray:
 	
-	def __init__(self) -> None:
+	def __init__(self, arr, k) -> None:
 		self.arr = arr
 		self.k = k
 		self.count = 0
 	
-	def check_sum(self):
-		print("Array: " + str(arr))
-		print("k = " + str(k))
-		sum_list = []
+	def check_sum(self) -> str:
+		if (len(self.arr) == 0):
+			return("Invalid array input")
+		sum_dict = []
 		sum = 0
 		for i in range(len(self.arr)):
 			sum += self.arr[i]
-			sum_list.append(sum)
+			sum_dict.append(sum)
 			if (sum == self.k):
 				self.count += 1
-			if (sum - self.k in sum_list):
-				self.count += sum_list.count(sum - self.k)
+			if (sum - self.k) in sum_dict:
+				self.count += sum_dict.count(sum - self.k)
 		return("Output = " + str(self.count))
-
-subArray = SubArray()
-print(subArray.check_sum())
--- Exercise/04/test_subarray.py
+++ Exercise/04/test_subarray.py
@@ -1,10 +1,36 @@
 from subarray import SubArray
+import unittest
+from data import arr, k
 
-class Test_SubArray:
+class TestSubArray(unittest.TestCase):
 	def test_instance_of_Subarray(self):
-		subarray = SubArray()
+		subarray = SubArray([1, 2, 3], 3)
 		assert(isinstance(subarray, SubArray))
 
-	def test_check_sum(self):
-		subarray = SubArray()
-		assert(subarray.check_sum() == "Output = 1023")
+	def test_check_empty_array(self):
+		subarray = SubArray([], 1)
+		self.assertEqual(subarray.check_sum(), "Invalid array input")
+	
+	def test_check_positive_array_elements(self):
+		subarray = SubArray([1, 2, 3], 3)
+		self.assertEqual(subarray.check_sum(), "Output = 2")
+	
+	def test_check_negative_array_elements(self):
+		subarray = SubArray([-1, -2, -3], -3)
+		self.assertEqual(subarray.check_sum(), "Output = 2")
+	
+	def test_check_mixed_array_elements(self):
+		subarray = SubArray([1, -1, 2, -2, 3, -3], 3)
+		self.assertEqual(subarray.check_sum(), "Output = 3")
+	
+	def test_check_k_equal_to_zero(self):
+		subarray = SubArray([1, -1, 1, -1], 0)
+		self.assertEqual(subarray.check_sum(), "Output = 8")
+
+	def test_check_positive_k(self):
+		subarray = SubArray([1, 2, 3], 3)
+		self.assertEqual(subarray.check_sum(), "Output = 2")
+
+	def test_check_negative_k(self):
+		subarray = SubArray(arr, k)
+		self.assertEqual(subarray.check_sum(), "Output = 1023")
