1421d21473eab3a393ab4403019996cc1bc86a8f
Omkar-Savoikar <mca.2104@unigoa.ac.in>
2023-02-05 17:40:30 +0530

    Refactor Execrise 02

--- Exercise/02/arrays.py
+++ Exercise/02/arrays.py
@@ -2,29 +2,26 @@ class ArrayTasks:
 	def __init__(self, arr) -> None:
 		self.arr = arr
 
+	def input_validation(self, start_pos, end_pos) -> bool:
+		return ((start_pos < 0) or (end_pos > len(self.arr)) or (start_pos > end_pos))
+
 	def element_at(self, index) -> int:
 		if index < 0:
 			return "Index out of range"
 		try:
 			return self.arr[index]
 		except IndexError:
-			return "Index out of range"
+			return(IndexError)
 	
 	def inclusive_range(self, start_pos, end_pos) -> list:
-		if (start_pos < 0) or (end_pos > len(self.arr)) or (start_pos > end_pos):
+		if self.input_validation(start_pos, end_pos):
 			return "Index out of range"
-		try:
-			return self.arr[start_pos:end_pos+1]
-		except IndexError:
-			return "Index out of range"
+		return self.arr[start_pos:end_pos+1]
 
 	def non_inclusive_range(self, start_pos, end_pos) -> list:
-		if (start_pos < 0) or (end_pos > len(self.arr)) or (start_pos > end_pos):
+		if self.input_validation(start_pos, end_pos):
 			return "Index out of range"
-		try:
-			return self.arr[start_pos:end_pos]
-		except IndexError:
-			return "Index out of range"
+		return self.arr[start_pos:end_pos]
 	
 	def start_and_length(self, start_pos, length) -> list:
 		if start_pos < 0:
@@ -37,4 +34,4 @@ class ArrayTasks:
 				op.append(self.arr[i])
 			return op
 		except IndexError:
-			return "Index out of range"
+			return(IndexError)
