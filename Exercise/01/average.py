8dad1f265a54afc96f105d9e6be828612d1b1581
Omkar-Savoikar <mca.2104@unigoa.ac.in>
2023-02-05 16:49:54 +0530

    Refactor Exercise 01

--- Exercise/01/average.py
+++ Exercise/01/average.py
@@ -2,20 +2,18 @@ import re
 
 class SerialAverage:
 	
-	def __init__(self, inp) -> None:
-		self.inp = inp
-
-	def average(self, num1, num2) -> float:
-		return (num1 + num2) / 2
+	def __init__(self, input) -> None:
+		self.input = input
 	
 	def string_split(self) -> list:
-		nums = [self.inp[:3], self.inp[4:9], self.inp[10:]]
-		return nums
+		return [self.input[:3], self.input[4:9], self.input[10:]]
 
 	def serial_average(self) -> str:
-		match = re.search(r'[0-9]{3}-[0-9]{2}.[0-9]{2}-[0-9]{2}.[0-9]{2}', self.inp)
+		match = re.search(r'[0-9]{3}-[0-9]{2}.[0-9]{2}-[0-9]{2}.[0-9]{2}', self.input)
 		if(not match):
 			return "Invalid input format"
 		nums = self.string_split()
-		avg = self.average(float(nums[1]), float(nums[2]))
-		return (nums[0]+"-%05.2f"% avg)
+		avg = (lambda x, y: ((x + y) /2))
+		average = avg(float(nums[1]), float(nums[2]))
+		# avg = self.average(float(nums[1]), float(nums[2]))
+		return (nums[0]+"-%05.2f"% average)
